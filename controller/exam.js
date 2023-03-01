a0bdec237f185bfc1b8aaa9e844eb7e5f62c3c2d
selimyurekli <root@192.168.1.100>
2023-03-01 14:49:58 +0300

    test and exam structure created

--- controller/exam.js
+++ controller/exam.js
@@ -1,7 +1,14 @@
-import test1 from "../test1.json"  assert { type: 'json' }
-
 
 const getTest = async function(req,res, next){
+
+    const test1 = {
+        "questions":[
+            {"index":"1","image":"questions/selim.jpeg","correctAnswer":"A", "subject":"Reel Sayılar"},
+            {"index":"2","image":"questions/selim.jpeg","correctAnswer":"A", "subject":"Reel Sayılar"},
+            {"index":"3","image":"questions/selim.jpeg","correctAnswer":"A", "subject":"Reel Sayılar"},
+            {"index":"4","image":"questions/selim.jpeg","correctAnswer":"A", "subject":"Reel Sayılar"}
+        ]
+    };
 
     const index = req.params.index;
     res.status(200).json({success:true, test:test1});
